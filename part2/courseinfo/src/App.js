6eacf51227ed8eda13dfec704246a927af9aa436
Marge <marge.consunji@gmail.com>
2022-03-23 16:38:27 +0800

    Setup content for exercises

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,60 +1,27 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map(({ name, exercises }) => (
-        <Part key={name} part={name} exercises={exercises} />
-      ))}
-    </>
-  );
-};
-
-const Total = ({ parts }) => {
-  return (
-    <p>
-      Number of exercises{' '}
-      {parts.reduce((prev, current) => prev + current.exercises, 0)}
-    </p>
-  );
-};
+import Course from './components/Course';
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1,
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2,
       },
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
-
-export default App;
