3f6bb9ca5b54fec7ce5c453f546a498f2e935671
Marge <marge.consunji@gmail.com>
2022-03-23 13:44:25 +0800

    Remove content meant for App.js

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,12 +1,5 @@
-import { useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
-  return <div>code here</div>;
-};
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
