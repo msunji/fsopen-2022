b33d62d55eb7ff6ff4bcc82ec1a3855a6410783d
Marge <marge.consunji@gmail.com>
2022-03-25 14:36:58 +0800

    Make event handler for adding name to phonebook
    - Add a new name to the phonebook
    - But also, check if the name's already been added - show an alert if so, otherwise, add it to the persons array

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,22 +3,49 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    const nameObj = {
+      name: newName
+    };
+
+    const matchNames = (name1, name2) => {
+      return name1.localeCompare(name2);
+    } 
+
+    if (persons.some(person => matchNames(person.name, nameObj.name) === 0)) {
+      alert(`${nameObj.name} has already been added to the phonebook`);
+    } else {
+      setPersons(persons.concat(nameObj));
+      setNewName('');
+    }
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        <ul>
+          { persons.map(person => <li key={person.name.replace(/ /g, "")}>{person.name}</li>)}
+        </ul>
+      </div>
     </div>
   )
 }
