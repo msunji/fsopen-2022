0e3731fbc0f408f9535991f562cee6678d1e0072
Marge <marge.consunji@gmail.com>
2022-04-13 13:28:38 +0800

    Fix effect hooks and API calls
    - Set units of measurement to metric for weather data - lest you end up with Kelvin units instead.
    - Get weather data for capital city, if the filtered list of countries only lists one single country.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,23 +1,48 @@
 import axios from 'axios';
+import Countries from './components/Countries';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [countries, setCountries] = useState([]);
-
-  const endpoint = 'https://restcountries.com/v3.1/all';
+  const [filter, setFilter] = useState('');
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
+    const endpoint = 'https://restcountries.com/v3.1/all';
     axios.get(endpoint).then((res) => {
-      console.log(res);
-      setCountries(res.data);
+      if (filter) {
+        const countryData = res.data.filter((country) =>
+          country.name.common.toLowerCase().includes(filter.toLowerCase())
+        );
+        setCountries(countryData);
+      }
     });
-  }, []);
+  }, [filter]);
+
+  useEffect(() => {
+    const endpoint = 'https://api.openweathermap.org/data/2.5/weather';
+    const openWeatherKey = process.env.REACT_APP_OPENWEATHER_APIKEY;
+    if (countries.length === 1) {
+      const [capital] = countries[0].capital;
+      console.log(capital);
+      axios
+        .get(`${endpoint}?q=${capital}&appid=${openWeatherKey}&units=metric`)
+        .then((res) => {
+          setWeather(res.data);
+        });
+    }
+  }, [countries]);
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
 
   return (
     <div>
       <div>
         <span>find countries</span>
-        <input type="text" />
+        <input type="text" value={filter} onChange={handleFilter} />
+        <Countries results={countries} weatherData={weather} />
       </div>
     </div>
   );
