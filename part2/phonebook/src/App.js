ce0d791f35ba5bc8623a2a91d8672f9d863c12f1
Marge <marge.consunji@gmail.com>
2022-04-13 17:46:59 +0800

    Add notification component

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
-import personsService from './services/persons';
-import axios from 'axios';
+import './styles/styles.css';
+import phonebook from './services/phonebook';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import PersonList from './components/PersonList';
+import Notification from './components/Notification';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
-
-  const endpoint = 'http://localhost:3001/persons';
+  const [msg, setMsg] = useState('');
+  const [err, setErr] = useState(false);
 
   useEffect(() => {
-    personsService.getPersons((persons) => {
+    phonebook.getPersons().then((persons) => {
       setPersons(persons);
     });
   }, []);
@@ -31,34 +32,89 @@ const App = () => {
     setFilter(event.target.value);
   };
 
+  const removeMsg = () => {
+    if (err) {
+      setTimeout(() => {
+        setMsg('');
+        setErr(false);
+      }, 4000);
+    } else {
+      setTimeout(() => {
+        setMsg('');
+      }, 4000);
+    }
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
 
     const personObj = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1,
+      id: new Date().getUTCMilliseconds(),
     };
 
-    const matchNames = (name1, name2) => {
-      return name1.localeCompare(name2);
-    };
+    const personName = persons.find(
+      (person) => person.name.toLowerCase() === personObj.name.toLowerCase()
+    );
+    const personNumber = persons.find(
+      (person) => person.number === personObj.number
+    );
 
-    if (
-      persons.some((person) => matchNames(person.name, personObj.name) === 0)
-    ) {
+    if (personName && personNumber) {
       alert(`${personObj.name} has already been added to the phonebook`);
+    } else if (personName && !personNumber) {
+      if (
+        window.confirm(
+          `${personObj.name} already exists. Would you like to replace the old number with a new one?`
+        )
+      ) {
+        phonebook
+          .updatePerson(personName.id, {
+            ...personName,
+            number: personObj.number,
+          })
+          .then((updatedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== personName.id ? person : updatedPerson
+              )
+            );
+            setMsg(`Successfully updated ${updatedPerson.name}'s number`);
+            removeMsg();
+            setNewName('');
+            setNewNumber('');
+          })
+          .catch((err) => {
+            setMsg(
+              `It looks like ${personName.name}'s already been removed from the phonebook.`
+            );
+            setErr(true);
+            removeMsg();
+          });
+      }
+      return;
     } else {
-      personsService.createPerson(personObj).then((newPerson) => {
+      phonebook.createPerson(personObj).then((newPerson) => {
         setPersons(persons.concat(newPerson));
+        setMsg(`Successfully added ${newPerson.name}`);
+        removeMsg();
         setNewName('');
         setNewNumber('');
       });
     }
   };
 
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      phonebook.deletePerson(id);
+      setPersons(persons.filter((person) => person.id !== id));
+    }
+    return;
+  };
+
   return (
-    <div>
+    <div className="phonebook-container">
       <h2>Phonebook</h2>
       <Filter filterVal={filter} onFilterChange={handleFilter} />
       <h2>Add a New Phonebook Entry</h2>
@@ -69,8 +125,13 @@ const App = () => {
         number={newNumber}
         handleNumber={handleNewNum}
       />
+      {msg && <Notification message={msg} error={err} />}
       <h2>Numbers</h2>
-      <PersonList persons={persons} filter={filter} />
+      <PersonList
+        persons={persons}
+        filter={filter}
+        deletePerson={deletePerson}
+      />
     </div>
   );
 };
