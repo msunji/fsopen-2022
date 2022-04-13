3ae59672981ebb4ecf98e54ed52410be2743e76e
Marge <marge.consunji@gmail.com>
2022-04-13 16:09:37 +0800

    Add function to delete name from phonebook

--- part2/phonebook/src/components/PersonList.js
+++ part2/phonebook/src/components/PersonList.js
@@ -1,13 +1,18 @@
-const PersonList = ({ persons, filter }) => {
+const PersonList = ({ persons, filter, deletePerson }) => {
   return (
     <ul>
-      {
-        persons
-          .filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
-          .map(({ name, number, id }) => <li key={id}>{name} {number}</li>)
-      }
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map(({ name, number, id }) => (
+          <li key={id}>
+            {name} {number}{' '}
+            <button onClick={() => deletePerson(id, name)}>delete</button>
+          </li>
+        ))}
     </ul>
-  )
-} 
+  );
+};
 
 export default PersonList;
