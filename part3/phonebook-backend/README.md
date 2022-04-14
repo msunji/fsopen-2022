a5d91d8c93fe2175882b8b1aed7e01bc004cc352
Marge <marge.consunji@gmail.com>
2022-04-14 16:23:00 +0800

    Complete exercises

--- part3/phonebook-backend/README.md
+++ part3/phonebook-backend/README.md
@@ -2,12 +2,27 @@
 
 ## Exercise 3.1
 
+- [x] Implement a Node app that returns a hardcoded list of phonebook entries from the address `http://localhost:3001/api/persons`.
+
 ## Exercise 3.2
+
+- [x] Implement a page at the address `http://localhost:3001/info` with information about: how many entries exist in the phonebook array and the time the request was received.
 
 ## Exercise 3.3
 
+- [x] Implement functionality for displaying phonebook info for a single entry. Use id as the parameter.
+
 ## Exercise 3.4
+
+- [x] Implement functionality that makes it possible to delete a single phonebook entry by making an HTTP DELETE request to said entry's URL.
 
 ## Exercise 3.5
 
+- [x] Expand the backend so new phonebook entries can be added thru HTTP POST requests to the address `http://localhost:3001/api/persons`. Generate a new id for the phonebook entry with the `Math.random` function.
+
 ## Exercise 3.6
+
+- [x] Implement error handling for creating new entries. A request won't be allowed to succeed if:
+
+1. The name or number is missing
+2. The name already exists in the phonebook
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
 const persons = [
   {
     id: 1,
@@ -25,8 +27,57 @@ const persons = [
   },
 ];
 
+const genId = () => {
+  return Math.floor(Math.random() * 88);
+};
+
+app.get('/info', (req, res) => {
+  const phonebookLen = persons.length;
+  const time = new Date();
+  const routeText = `<p>Phonebook has info for ${phonebookLen} people. <br />
+                    ${time}
+                    </p>`;
+  res.send(routeText);
+});
+
 app.get('/api/persons', (req, res) => {
   res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => {
+    return person.id === id;
+  });
+  res.json(person);
+});
+
+app.post('/api/persons', (req, res) => {
+  const person = req.body;
+
+  const existing = persons.some(
+    (entry) => entry.name.toLowerCase() === person.name.toLowerCase()
+  );
+
+  if (!person.name || !person.number) {
+    return res.status(400).json({
+      error: 'Missing name or number',
+    });
+  }
+  if (existing) {
+    return res.status(400).json({
+      error: 'Entry already exists in phonebook',
+    });
+  }
+  person.id = genId();
+  res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  person = persons.filter((person) => person.id !== id);
+  res.json(person);
+  res.status(204).end();
 });
 
 app.listen(port, () => {
