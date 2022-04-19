3094c10dc694daf8cf8a006004d26cd003aa8a34
Marge <marge.consunji@gmail.com>
2022-04-19 17:32:35 +0800

    Add env var for PORT - don't fix to 3001

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('client'));
 app.use(express.json());
@@ -92,6 +92,6 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on PORT ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`);
 });
