72653bf0a489b21064493ffc26e4ed921ceaad68
Marge <marge.consunji@gmail.com>
2022-04-22 15:33:01 +0800

    Set custom error message to return on ValidationError

--- part3/phonebook-backend/middleware/errorHandler.js
+++ part3/phonebook-backend/middleware/errorHandler.js
@@ -2,6 +2,9 @@ const errorHandler = (err, req, res, next) => {
   console.log(err.message);
   if (err.name === 'CastError') {
     return res.status(400).send({ error: 'Malformatted id' });
+  } else if (err.name === 'ValidationError') {
+    let { errors } = err;
+    return res.status(400).json({ error: errors.name.message });
   }
   next(err);
 };
