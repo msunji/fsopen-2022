18705075070f36af41e8339f524ac831a5641803
Marge <marge.consunji@gmail.com>
2022-04-22 15:15:07 +0800

    Use CORS middleware to correct same origin policy related err

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const logger = require('./middleware/logger');
 const Person = require('./models/person');
 const errorHandler = require('./middleware/errorHandler');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.static('client'));
 app.use(express.json());
 app.use(logger);
