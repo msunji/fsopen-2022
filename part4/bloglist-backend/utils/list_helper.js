bf6652ff24a6960f9650741225d8a9686a8c96b2
Marge <marge.consunji@gmail.com>
2022-04-28 09:51:49 +0800

    Fix typo in favoriteBlog function

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+const { info } = require('../utils/logger');
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -10,7 +12,20 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    if (prev.likes > current.likes) {
+      return { title: prev.title, author: prev.author, likes: prev.likes };
+    }
+    return current;
+  };
+  info(blogs.reduce(reducer, blogs[0]));
+
+  return blogs.length === 0 ? null : blogs.reduce(reducer, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
