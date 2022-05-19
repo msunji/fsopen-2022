3b5a4eb1849ac665db7f401a6abf18e3928ed650
Marge <marge.consunji@gmail.com>
2022-05-19 16:16:23 +0800

    Import express-async-errors

--- part4/bloglist-backend/app.js
+++ part4/bloglist-backend/app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config');
 const express = require('express');
+require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
@@ -9,12 +10,15 @@ const mongoose = require('mongoose');
 
 const url = config.MONGODB_URI;
 
-mongoose
-  .connect(url)
-  .then((res) => info('Connected to db'))
-  .catch((err) => {
-    error('error: ', err);
-  });
+// Set this up so that you don't connect to the actual database in testing
+if (process.env.NODE_ENV !== 'test') {
+  mongoose
+    .connect(url)
+    .then((res) => info('Connected to db'))
+    .catch((err) => {
+      error('error: ', err);
+    });
+}
 
 app.use(cors());
 app.use(express.json());
