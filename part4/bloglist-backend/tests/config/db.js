9767cfb5f9242134074a52c858f68e9ae1c12df9
Marge <marge.consunji@gmail.com>
2022-05-20 14:50:00 +0800

    Make function to clear database

--- part4/bloglist-backend/tests/config/db.js
+++ part4/bloglist-backend/tests/config/db.js
@@ -4,6 +4,9 @@ const { error } = require('../../utils/logger');
 
 let testDB;
 
+// Get client
+const client = mongoose.connection.getClient();
+
 // Connect to test DB
 const connect = async () => {
   await mongoose.disconnect();
@@ -12,6 +15,7 @@ const connect = async () => {
   const testUri = testDB.getUri();
 
   await mongoose.connect(testUri, (err) => {
+    console.log('Connected to test db');
     if (err) {
       error(err);
     }
@@ -24,7 +28,18 @@ const close = async () => {
   await testDB.stop();
 };
 
+// Clear database
+const clear = async () => {
+  const collections = await mongoose.connection.collections;
+
+  for (let key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+};
+
 module.exports = {
   connect,
   close,
+  clear,
 };
