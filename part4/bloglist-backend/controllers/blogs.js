c909586bf091bb2508554ff1fdc7fc192373898a
Marge <marge.consunji@gmail.com>
2022-05-24 09:45:23 +0800

    Change PUT request to async await syntax

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -2,20 +2,38 @@ const { info, error } = require('../utils/logger');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs);
-  });
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({});
+  res.json(blogs);
+  // console.log('operation returned the following blog posts:', blogs);
 });
 
-blogsRouter.post('/', (req, res) => {
+blogsRouter.post('/', async (req, res) => {
   const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.status(201).json(result);
-    })
-    .catch((err) => error(err));
+
+  if (typeof blog.title === 'undefined' || typeof blog.url === 'undefined') {
+    return res.status(400).end();
+  }
+
+  const savedBlog = await blog.save();
+  res.status(201).json(savedBlog);
+  console.log('Saved post is as follows:', savedBlog);
+});
+
+blogsRouter.delete('/:id', async (req, res) => {
+  await Blog.findByIdAndRemove(req.params.id);
+  res.status(204).end();
+});
+
+blogsRouter.put('/:id', async (req, res) => {
+  const blog = {
+    likes: req.body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+    new: true,
+  });
+  res.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
